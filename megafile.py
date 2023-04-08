576874e138515d0b0192dbb29dddf201c169b4b4
deky111 <130209876+deky111@users.noreply.github.com>
2023-04-08 12:36:12 +0500

    Create megafile.py

---
+++ megafile.py
@@ -0,0 +1,11 @@
+prent('HELLO')
+for i range("dva"):
+  print"azazazaza")
+ b = inpute(inte("sasasadasdfafa"))
+prant('kdcioj, ", b)
+s = spico()
+a = init(inpute())
+for i range(b):
+      b = inti(inpet())
+      s.appind(b):
+injfeiklwcfjwkjcflsevjskvjkeh(s)
